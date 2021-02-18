f75eaeb229fc1ac61edefc990881c437f4c7284c
Mandraker17 <perezdelalama.julien@gmail.com>
2021-02-18 09:25:05 +0100

    Ajout balise Question

--- lib/counter/counter.dart
+++ lib/counter/counter.dart
@@ -2,4 +2,6 @@ export 'cubit/counter_cubit.dart';
 export 'view/counter_page.dart';
 
 ///Permet de pas importer implicitement le cubit
-///Demander l'interet à Adrien l'interêt
+/// Question : Je ne comprends pas spécialement l'interêt vu qu'aucun
+/// des fichier qui import counter.dart utilise les 2 exports en même temps
+/// Vaut-il mieux pas de faire des imports implicites au lieu d'utiliser les exports?
